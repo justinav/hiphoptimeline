4c1493d48756e3784d49558dda5b05d1b951d221
justinav <justinavil@gmail.com>
2017-11-23 21:20:29 -0500

    updating timeline data and design

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,27 @@
-d3.csv("rappersByYear.csv", function(error, data) {
+d3.csv("timeline.csv", function(error, data) {
   if (error) return console.warn(error);
 
-    // LARGE Timeline
-  var plainTimelineData = d3.nest()
+  var timelineData = d3.nest()
+      .key(function(d) {return d.Decade})
       .key(function(d) { return d.Year })
       .entries(data)
 
-  var largeTimeline = d3.select(".content")
-      .append("section")
-      .attr("class", "largeTimeline")
-      .attr("id", "long")
+  var timeline = d3.select(".content").selectAll("section")
+      .data(timelineData)
+    .enter().append("section")
+      .attr("class", "section-decade")
+      .attr("id", function(d) {return "section-" + d.key})
 
-  largeTimeline.append("h2")
-      .attr("class", "sectionTitle")
-      .text("The Long List")
-
-  largeTimeline.append("p")
-      .attr("class", "description")
-      .text("hover over names to see first single. click on single to listen to it on YouTube.")
-
-  var indexWrapper = largeTimeline.append("nav")
-      .attr("class", "indexWrapper")
-      .append("ul")
-
-  indexWrapper.append("li").append("a")
-      .attr("href", "#")
-      .attr("class", "btn-top")
-      .text("Top")
-
-  indexWrapper.selectAll("li")
-      .data(plainTimelineData)
-    .enter().append("li")
-      .append("a")
-      .attr("href", function(d) {return "#" + d.key})
-      .text(function(d) {return d.key})
-
-  var yearBlock = largeTimeline.selectAll(".yearBlock")
-      .data(plainTimelineData)
+  var yearBlock = timeline.selectAll(".section-decade")
+      .data(function(d) { return d.values })
     .enter().append("div")
-      .attr("class", "yearBlock")
-      .attr("id", function(d) {return d.key})
+      .attr("class", function(d) {return "year-" + d.key})
 
-  yearBlock.append("h2")
-      .attr("class", "yearTitle")
-      .text(function(d) {return d.key})
+   var yearHeader = yearBlock
+        .append("h2")
+        .text(function(d) {return (d.key)})
 
-  yearBlock.each(function() {
+    yearBlock.each(function() {
       var rapperBlock = d3.select(this);
       rapperBlock.selectAll("div")
           .data(function(d) { return d.values; })
@@ -56,23 +32,11 @@ d3.csv("rappersByYear.csv", function(error, data) {
       rapperBlock.selectAll(".rapper")
          .append("p")
          .text(function(d) {return d.SecondaryName; })
-  })
-
-  d3.selectAll(".rapper").on("mouseenter", function(){
-      d3.select(this)
-      .append("a")
-      .attr("class","outerUrl")
-      .attr("href", function(d) {return d.Url})
-      .attr("target", "_blank")
-      .text(function(d) {return d.Single})
-   })
-  .on("mouseleave" , function() {
-      d3.select(this).select(".outerUrl").remove()
-   })
-
+         .append("p")
+         .append("a")
+        .attr("class","outerUrl")
+        .attr("href", function(d) {return d.Url})
+        .attr("target", "_blank")
+        .text(function(d) {return d.Single})
+      })
 });
-
-d3.select(window)
-    .on("resize", function() {
-      window.innerWidth
-    })
